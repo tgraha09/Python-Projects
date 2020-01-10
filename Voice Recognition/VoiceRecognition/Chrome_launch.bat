32695a2cd646fb58228f116e1e24b13ea5841677
tgraha09 <tfire09@gmail.com>
2020-01-09 20:21:03 -0500

    test

--- Voice Recognition/VoiceRecognition/Chrome_launch.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start" " "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
--- Voice Recognition/VoiceRecognition/commands.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start """C:\Users\Taurian Graham\Documents\Projects\Personal\Python-Projects\Voice Recognition&Personal Assiistant"
